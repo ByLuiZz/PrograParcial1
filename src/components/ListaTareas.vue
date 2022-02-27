9be09e7b690c33f1b5d596df06989548226f907b
ByLuiZz <98935259+ByLuiZz@users.noreply.github.com>
2022-02-27 09:09:33 -0600

    Actualizacion

---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,36 @@
+<template>
+  <form id="app" @submit.prevent="createTask">
+    <label class="label" for="task">Nueva tarea:</label>
+    <input type="text" v-model="newTask" id="task" />
+    <input type="submit" value="Crear tarea" />
+    <ul>
+      <li v-for="(task, i) in tasks" :key="'task' + i" >{{task.text}}</li>
+    </ul>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    newTask: "",
+    tasks: []
+  }),
+  methods: {
+    createTask() {
+      let task = {
+        text: this.newTask,
+        completed: true
+      };
+      this.tasks.push(task);
+      this.newTask = "";
+      console.log(this.tasks);
+    }
+  }
+};
+</script>
+<style scoped>
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+</style>
---
+++ src/views/ViewTareas.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=container>
+    <h3>
+      Ingresando Tarea
+      <ListaTareas/>
+    </h3>
+
+  </div>
+</template>
+
+<script>
+import ListaTareas from '@/components/ListaTareas.vue'
+export default {
+  name: 'ViewTareas',
+  components: {ListaTareas
+  }
+}
+</script>
+
