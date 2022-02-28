43102b6c6e01bcb805acbf8684a13bbd3c3ec4c3
JhonatanJosueBenitezTurcios <97970018+JhonatanJosueBenitezTurcios@users.noreply.github.com>
2022-02-27 20:09:10 -0600

    Calculadora

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ViewTareas from '../views/ViewTareas.vue'
+import ViewCalculadora from '../components/ViewCalculadora'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: ViewTareas
+  },
+  {
+    path: '/calculadora',
+    name: 'calculadora',
+    component: ViewCalculadora
   }
 ]
 
---
+++ src/views/ViewCalculadora.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="calculadora">
+        
+        <Calculadora/>
+    </div>
+</template>
+
+
+<script>
+
+import Calculadora from '../components/Calculadora.vue'
+
+
+export default {
+    name: 'ViewCalculadora',
+    components: {
+        
+        Calculadora
+    }
+}
+</script>
